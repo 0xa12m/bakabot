e61d26d674f7f0d9ab2c6bd0e315ef354c294ed7
inwarm <inwarmtanoshi@gmail.com>
2018-01-23 16:10:19 +0700

    fix code

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def makeWebhookResult(data):
     # print(json.dumps(item, indent=4))
 
     tempF = condition.get('temp')
-    #tempC = ( int(tempF) - 32 )/(1.8)
+    float(tempC) = ( float(tempF) - 32 )/(1.8)
 
     speech = "เมือง " + location.get('city') + \
              ", มีอุณหภูมิอยู่ที่  " + condition.get('temp') + " " + units.get('temperature')
